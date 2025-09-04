abc373f65bb0a64d20f7f7fc5ee04f05aa209227
debopriyadebnath <debopriyadebnath121@gmail.com>
2025-09-05 00:33:08 +0530

    Add vectorstore loading and PDF upload handling functionality

--- server/modules/load_vecroestore.py
+++ server/modules/load_vecroestore.py
@@ -20,4 +20,61 @@ os.environ["GOOGLE_API_KEY"]= GOOGLE_API_KEY
 UPLOAD_DIR="./uploaded_docs"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+#initialize pinecone instance
+pc=Pinecone(api_key=PINECONE_API_KEY)
+spec=ServerlessSpec(cloud="aws",region=PINECONE_ENV)
+existing_indexes= [i["name"]for i in pc.list_indexces()]
+
+if PINECONE_INDEX_NAME not in existing_indexces:
+    pc.create_index(
+        name= PINECONE_INDEX_NAME,
+        dimension=768,
+        metric="dotproduct",
+        spec=spec
+    )
+
+    while not pc.describe_index(PINECONE_INDEX_NAME).status["ready"]:
+        time.sleep(1)
+index=pc.Index(PINECONE_INDEX_NAME)
+
+def load_vectorstore(uploaded_files):
+    embed_model= GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    file_paths=[]
+
+    #1.upload
+    for file in uploaded_files:
+        save_path=Path(UPLOAD_DIR)/file.filename
+        with open(save_path,"wb")as f:
+            f.write(file.file.read())
+        file_paths.append(str(save_path))
+
+    #2.split
+    for file_path in file_path:
+        loader=PyPDFLoader(file_path)
+        documents=loader.load()
+
+        splitter= RecursiveCharacterTextSplitter(chunk_size= 500, chunk_overlap=100)
+        chunks=splitter.split_documents(documents)
+
+        texts=[chunk.page_content for chunk in chunks]
+        metadata=[chunk.metadata for chunk in chunks]
+        ids= [f"{Path(file_path).stem}-{i}"for i in range (chunks)]
+    #3.Embedding
+        print(f"Embedding chunks")
+        embedding=embed_model.embed_documents(texts)
+    
+    #4.upsert
+    print("Upserting embeddings...")
+    with tqdm(total=len(embedding),desc="Upserting to Pinecone")as progress:
+        index.upsert(vectors=zip(ids,embedding,metadata))
+        progress.update(len(embedding))
+    print(f"upload complete for {file_path}")
+
+
+
+
+
+
+
+
 
---
+++ server/routes/uploads_pdf.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter, UploadFile, File
+from typing import List
+from modules.load_vectorstore import load_vectorstore
+from fastapi.responses import JSONResponse
+from logger import logger
